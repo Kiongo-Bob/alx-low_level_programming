6eaa1889fc1e2791e1256abc62ba1a4e7400fc39
Kiongo-Bob <njoguub@gmail.com>
2023-07-13 13:12:46 -0700

    Hello, world!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * Description: File to print simple stuff
+ *
+ * Retun: 0
+ *
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return 0;
+}
